af2e0d293b6fc432dd3186433cb4ce050b07fdef
tuana.kursun <tuana.kursun@TUANA-KURSUN>
2021-08-26 11:14:10 +0300

    update

--- CodeFirstProject/Controllers/OrderController.cs
+++ CodeFirstProject/Controllers/OrderController.cs
@@ -11,36 +11,64 @@ namespace CodeFirstProject.Controllers
     public class OrderController : Controller
     {
         private readonly DbProjectContext _dbProjectContext;
-        private Order order;
+        private Order OrderId;
 
         public OrderController(DbProjectContext dbProjectContext)
         {
             _dbProjectContext = dbProjectContext;
         }
+
         public IActionResult Index()
         {
             var model = _dbProjectContext.Orders.ToList();
             return View(model);
+           
+           
         }
 
-        public IActionResult Create()//gerek var mı?
-        {
+        public IActionResult Create() { 
             return View();
         }
         [HttpPost]
         public IActionResult Create(Order order)
         {
+
             _dbProjectContext.Orders.Add(order);
             _dbProjectContext.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int id)
+        public IActionResult Delete(int Id)
         {
-            var model = _dbProjectContext.Orders.Find(id);
+            var order = _dbProjectContext.Orders.Find(Id);
             _dbProjectContext.Orders.Remove(order);
             _dbProjectContext.SaveChanges();
             return RedirectToAction("Index");
+
         }
+
+        public IActionResult Edit(int id)
+        {
+            var order = _dbProjectContext.Orders.Find(id);
+            if (order != null)
+            {
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public IActionResult Edit(Order order)
+
+        {
+            _dbProjectContext.Update(order);
+            _dbProjectContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
-}
+
+    }
+
